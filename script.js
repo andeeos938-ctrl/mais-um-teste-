a6f8eff8a307a8377f9ee137dc669fd0ad5b025a
andeeos938-ctrl <emailandeeos938@gmail.com>
2026-03-27 17:43:30 -0300

    alteraçao 2

--- script.js
+++ script.js
@@ -25,3 +25,48 @@ function addTask() {
             renderTasks();
         }
 }
+
+function renderTasks(filter="all") {
+     const list= document.getElementById("taskList");
+        lister.innerHTML = "";
+
+        tasks.forEach((task, index) => {
+            if (
+                filter === "pending" && task.done ||
+                filter === "done"   && !task.done
+            ) return;
+
+            const li = document.createElement("li");
+            li.classList.add("task-priority");
+
+                if (task.done) li.classList.add("done");
+               
+                li.innerHTML = `
+                <span onclick="toggleTask(${index})">${task.text} (${task.priority})</span>
+                <div>
+                    <button onclick="editTask(${index})">❌</button>
+                    </div>
+            `;
+            list.appendChild(li);
+        }
+                
+            );
+}
+
+function toggleTask(index) {
+    tasks[index].done = !tasks[index].done;
+    saveTasks();
+    renderTasks();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    saveTasks();
+    renderTasks();
+}
+
+function filterTasks(filter) {
+    renderTasks(filter);
+}
+
+renderTasks();
